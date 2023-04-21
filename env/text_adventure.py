59e300dd7d0a689b29b023efff83cbb22a5042c7
Kenny Jewett <kennethrjewett@gmail.com>
2023-04-21 16:41:55 -0500

    finished sounds and logging

--- env/text_adventure.py
+++ env/text_adventure.py
@@ -1,6 +1,9 @@
 from functools import reduce
 from time import sleep
 import re
+from playsound import playsound
+import logging
+
 
 # def my_func(params):
 #     print(f"I love {params}")
@@ -9,9 +12,11 @@ import re
 
 
 # *sleep(seconds)*
-
+logging.basicConfig(filename="logs.txt", encoding="utf-8", level=logging.INFO)
 
 def introduction():
+    sleep(1)
+    playsound("./creepy-cave-47164.mp3")
     print("   __________________________________________________________________________________")
     print(" / \                                                                                 \.")
     print("|   |                                                                                |.")
@@ -23,8 +28,8 @@ def introduction():
     print("    |                         when you're ready to begin.                            |.")
     print("    |                                                                                |.")
     print("    |                                                                                |.")
-    print("    |                                                                                |.")
-    print("    |                                                                                |.")
+    print("    |                    WARNING: Some sounds are a little loud.                     |.")
+    print("    |                   You might want to turn your volume down a little             |.")
     print("    |                                                                                |.")
     print("    |                                                                                |.")
     print("    |                                                                                |.")
@@ -33,10 +38,12 @@ def introduction():
     print("    \_/dc__________________________________________________________________  __________/.")
     print("                                                                                         ")
     print("                                                                                         ")
+    
 
     global player_name
 
     player_name = input("Enter you name here: ")
+    logging.info({player_name})
 
     cave_start(player_name)
 
@@ -63,9 +70,10 @@ def cave_start(player_name):
     print(f"    \_/________________________________________________________________________________/.")
     print(f"                                                                                         ")
     print(f"                                                                                         ")
+    playsound('./quake-and-break-99034.mp3')
 
     cave_choice = input("What do you do? ")
-
+    logging.info({cave_choice})
     clean_input = cave_choice.lower()
     if re.search("left", clean_input):
         cave_pit_fail()
@@ -84,28 +92,33 @@ def cave_pit_fail():
     print(f" \_ |                                                                                |.")
     print(f"    |               You wake up in a dark cave with a huge headache.                 |.")
     print(f"    |                                                                                |.")
-    print(f"    |               You are surrounded by the bones of dead animals                  |.")
-    print(f"    |               in a large pit with only a small shaft of light                  |.")
-    print(f"    |               peeking through a crack in the roof of the cave.                 |.")
+    print(f"    |               You are back in the pit.                                         |.")
+    print(f"    |               Your head hurts a lot. You must have hit it on something.        |.")
     print(f"    |                                                                                |.")
-    print(f"    |               There are two passages in fron of you if you                     |.")
+    print(f"    |                                                                                |.")
+    print(f"    |               There are two passages in front of you if you                    |.")
     print(f"    |               choose to CLIMB out of the pit.                                  |.")
     print(f"    |                                                                                |.")
     print(f"    |               You should probably CLIMB out of the pit before you try          |.")
     print(f"    |               to do anything else.                                             |.")
-    print(f"    |               WHAT DO YOU DO?                                                  |.")
+    print(f"    |                                WHAT DO YOU DO?                                 |.")
     print(f"    |   _____________________________________________________________________________|___")
     print(f"    |  /                                                                                /.")
     print(f"    \_/________________________________________________________________________________/.")
     print(f"                                                                                         ")
     print(f"                                                                                         ")
+    playsound('./quake-and-break-99034.mp3')
 
     cave_choice = input("What do you do this time? ")
+
+    logging.info({cave_choice})
 
     clean_input = cave_choice.lower()
     if re.search("climb", clean_input):
         cave_pit_success()        
     else: 
+        sleep(1)
+        playsound('./quake-and-break-99034.mp3')
         input("You fall and impale yourself on a jagged animal bone! ||RIP|| PRESS ENTER TO TRY AGAIN "), introduction()    
     
 
@@ -134,6 +147,8 @@ def cave_pit_success():
         print(f"                                                                                         ")
    
         direction_choice = input("What do you do? ")
+
+        logging.info({direction_choice})
 
         clean_input = direction_choice.lower()
         if re.search("left", clean_input):
@@ -168,6 +183,8 @@ def cave_path_left():
    
         left_entrance = input("What do you do? ")
 
+        logging.info({left_entrance})
+
         clean_input = left_entrance.lower()
         if re.search("wait", clean_input):
             cave_left_wait()
@@ -182,6 +199,7 @@ def cave_path_left():
 
 def cave_left_wait():
         sleep(1)
+        playsound("./halloween-horror-sound-lurking-monster-145070.mp3")
         print(f"   __________________________________________________________________________________")
         print(f" / \                                                                                 \.")
         print(f"|   |                                                                                |.")
@@ -207,24 +225,27 @@ def cave_left_wait():
 
         snake_palindrome = input("What do you do? ")
 
+        logging.info({snake_palindrome})
+
         clean_str = reduce(lambda x, y: x + y, snake_palindrome.split(" "))
 
         if clean_str[::-1] == clean_str and len(clean_str) > 4:
              escape_cave()
         else: 
-            sleep(1) 
+            sleep(2)
+            playsound("./fuzzy-jumpscare-80560.mp3")
             print(f"   __________________________________________________________________________________")
             print(f" / \                                                                                 \.")
             print(f"|   |                                                                                |.")
             print(f" \_ |                                                                                |.")
             print(f"    |               The snake laughs and says: I think I might be hungry for         |.")
-            print(f"    |               a snack after all.  Never odd or even.                           |.")
+            print(f"    |               a snack after all.                                               |.")
             print(f"    |                                                                                |.")
             print(f"    |               The snake rears up and strikes at you and everything             |.")
             print(f"    |               goes dark.                                                       |.")
             print(f"    |                                                                                |.")
-            print(f"    |               Yes, the giant rock sitting in the sun was a giant snake.        |.")
-            print(f"    |               Maybe you should have waited for your eyes to adjust.            |.")
+            print(f"    |                Never odd or even.                                              |.")
+            print(f"    |                                                                                |.")
             print(f"    |                                                                                |.")
             print(f"    |                                                                                |.")
             print(f"    |                                                                                |.")
@@ -235,24 +256,28 @@ def cave_left_wait():
             print(f"                                                                                         ")
             print(f"                                                                                         ")
 
-            sleep(1)
+            sleep(5)
+
             input("You died! ||RIP|| PRESS ENTER TO START AGAIN")
             introduction()
 
 def escape_cave():
+     playsound("./throat-singing-in-a-cave-25481.mp3")
      print("The snake lets you pass, and you see a way out of the cave.")
      input("Congrats!!! You escaped the cave!!! THE END! PRESS ENTER TO PLAY AGAIN")
+     logging.info(f"{player_name} beat the game")
      introduction()
         
 
 def cave_left_chamber():
         sleep(1) 
+        playsound("./fuzzy-jumpscare-80560.mp3")
         print(f"   __________________________________________________________________________________")
         print(f" / \                                                                                 \.")
         print(f"|   |                                                                                |.")
         print(f" \_ |                                                                                |.")
-        print(f"    |               The snake laughs and says: I think I might be hungry for         |.")
-        print(f"    |               a snack after all.  Never odd or even.                           |.")
+        print(f"    |                                                                                |.")
+        print(f"    |                                                                                |.")
         print(f"    |                                                                                |.")
         print(f"    |               The snake rears up and strikes at you and everything             |.")
         print(f"    |               goes dark.                                                       |.")
@@ -270,6 +295,7 @@ def cave_left_chamber():
         print(f"                                                                                         ")
 
         input("You died ||RIP|| PRESS ENTER TO TRY AGAIN")
+        logging.info(f"{player_name} died")
         introduction()
 
 
@@ -300,7 +326,10 @@ def cave_path_right():
     print(f"                                                                                         ")
     print(f"                                                                                         ")
     
+    
     right_passage = input("What do you do? ")
+
+    logging.info({right_passage})
 
     clean_input = right_passage.lower()
     if re.search("forward", clean_input):
@@ -332,14 +361,18 @@ def cave_path_right_failure():
         print(f"    \_/________________________________________________________________________________/.")
         print(f"                                                                                         ")
         print(f"                                                                                         ")
+        playsound('./quake-and-break-00934.mp3')
 
         pit_climb = input("What do you do this time? ")
+
+        logging.info({pit_climb})
 
         clean_input = pit_climb.lower()
         if re.search("climb", clean_input):
             cave_pit_success()        
         else:
             sleep(1) 
+            playsound('./quake-and-break-00934.mp3')
             input("You fall and impale yourself on a jagged animal bone! ||RIP|| PRESS ENTER TO TRY AGAIN "), introduction()
 sleep(1)
 introduction()
